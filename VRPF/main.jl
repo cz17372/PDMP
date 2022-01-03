4e9c1d89d8619a83763a47c0c2e8fe0a9714d0cf
Chang Zhang <chang.zhang0220@icloud.com>
2022-01-03 22:16:07 +0800

    Create Data Folder

--- VRPF/main.jl
+++ VRPF/main.jl
@@ -1,5 +1,5 @@
 cd(dirname(@__FILE__))
-using Distributions, Plots, StatsPlots,Random, StatsBase, LinearAlgebra
+using Distributions, Plots, StatsPlots,Random, StatsBase, LinearAlgebra, JLD2
 using Base:@kwdef
 theme(:ggplot2)
 include("ChangePoint.jl")
@@ -8,3 +8,7 @@ include("VRPF.jl")
 ξ,y = ChangePoint.SimData(seed=124)
 T = collect(0:100:1000)
 ChangePoint.ProcObsGraph(ξ,y)
+
+R = VRPF.PG(ChangePoint,y,T,method="Global",SMCN=5)
+
+@save "../data/ChangePoint_data.jld2" ξ y
