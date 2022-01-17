1a9e4c4b7117723deea647a25586731a9c5f14af
Chang Zhang <chang.zhang0220@icloud.com>
2022-01-17 15:01:11 +0800

    correct error

--- parmain.jl
+++ parmain.jl
@@ -8,7 +8,7 @@ println("Loading data...")
 ξ,y = ChangePoint.SimData(seed=2022);
 @save "data.jld2" y
 @everywhere @load "data.jld2"
-VRPF_RES_25par500fold = SharedArray{Float64}(50000,5,20)
+VRPF_RES_25par_500fold = SharedArray{Float64}(50000,5,20)
 println("Running VRPF with 25 particles, 500 fold")
 @sync @distributed for n = 1:20
     VRPF_RES_25par_500fold[:,:,n] = VRPF.PG(ChangePoint,y,collect(0.0:100.0:1000.0),SMCAdaptN=100,SMCN=25,Globalalpha=0.25)
@@ -19,7 +19,7 @@ BVRPF_RES_25par_500fold = SharedArray{Float64}(50000,5,20)
     BVRPF_RES_25par_500fold[:,:,n] = BlockVRPF.PG(ChangePoint,y,collect(0.0:100.0:1000.0),auxpar=[2.0,1.0],SMCAdaptN=100,SMCN=25,Globalalpha=0.25)
 end
 
-VRPF_RES_10par500fold = SharedArray{Float64}(50000,5,20)
+VRPF_RES_10par_500fold = SharedArray{Float64}(50000,5,20)
 println("Running VRPF with 10 particles, 500 fold")
 @sync @distributed for n = 1:20
     VRPF_RES_10par_500fold[:,:,n] = VRPF.PG(ChangePoint,y,collect(0.0:100.0:1000.0),SMCAdaptN=100,SMCN=10,Globalalpha=0.25)
@@ -30,7 +30,7 @@ BVRPF_RES_10par_500fold = SharedArray{Float64}(50000,5,20)
     BVRPF_RES_10par_500fold[:,:,n] = BlockVRPF.PG(ChangePoint,y,collect(0.0:100.0:1000.0),auxpar=[2.0,1.0],SMCAdaptN=100,SMCN=10,Globalalpha=0.25)
 end
 
-VRPF_RES_50par500fold = SharedArray{Float64}(50000,5,20)
+VRPF_RES_50par_500fold = SharedArray{Float64}(50000,5,20)
 println("Running VRPF with 50 particles, 500 fold")
 @sync @distributed for n = 1:20
     VRPF_RES_50par_500fold[:,:,n] = VRPF.PG(ChangePoint,y,collect(0.0:100.0:1000.0),SMCAdaptN=100,SMCN=50,Globalalpha=0.25)
@@ -38,10 +38,10 @@ end
 println("Running Block VRPF with 50 particles, 500 fold")
 BVRPF_RES_50par_500fold = SharedArray{Float64}(50000,5,20)
 @sync @distributed for n = 1:20
-    BVRPF_RES_55par_500fold[:,:,n] = BlockVRPF.PG(ChangePoint,y,collect(0.0:100.0:1000.0),auxpar=[2.0,1.0],SMCAdaptN=100,SMCN=50,Globalalpha=0.25)
+    BVRPF_RES_50par_500fold[:,:,n] = BlockVRPF.PG(ChangePoint,y,collect(0.0:100.0:1000.0),auxpar=[2.0,1.0],SMCAdaptN=100,SMCN=50,Globalalpha=0.25)
 end
 
-VRPF_RES_100par500fold = SharedArray{Float64}(50000,5,20)
+VRPF_RES_100par_500fold = SharedArray{Float64}(50000,5,20)
 println("Running VRPF with 100 particles, 500 fold")
 @sync @distributed for n = 1:20
     VRPF_RES_100par_500fold[:,:,n] = VRPF.PG(ChangePoint,y,collect(0.0:100.0:1000.0),SMCAdaptN=100,SMCN=100,Globalalpha=0.25)
