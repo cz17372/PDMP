710dfd408c4f748e117ba61a95acd881e28a9222
Chang Zhang <chang.zhang0220@icloud.com>
2022-03-08 21:36:38 +0800

    asdf

--- main_changepoint.jl
+++ main_changepoint.jl
@@ -1,5 +1,6 @@
 cd(dirname(@__FILE__))
 using Distributions, Plots, StatsPlots,Random, StatsBase, LinearAlgebra, JLD2,Random; using Base:@kwdef;
+using Measures
 theme(:wong2)
 include("VRPF.jl");include("SNC.jl");include("BlockVRPF.jl")
 
@@ -14,3 +15,11 @@ BlockVRPF_10Particles = BlockVRPF.PG(SNC,y,T,proppar=(λ,Σ),θ0 = θ0,auxpar=[2
 VRPF_10Particles = VRPF.PG(SNC,y,T,proppar=(λ,Σ),θ0 = θ0,SMCN=10)
 
 plot(BlockVRPF_10Particles[:,3])
+
+
+t = collect(0:0.1:1000)
+ζ = SNC.getζ.(t,Ref(J),Ref(SNC.pars()))
+p1 = plot(t,ζ,label="",color=:darkolivegreen,framestyle=:box,xlabel="t",ylabel="\\zeta",size=(1200,400),margin=15pt,linewidth=3.0)
+p2 = histogram(y,bins=400,label="",xlabel="t",ylabel="Frequency",size=(1200,400),framestyle=:box)
+plot(p1,p2,layout=(2,1),size=(1200,800))
+savefig("SNC_Data.pdf")
